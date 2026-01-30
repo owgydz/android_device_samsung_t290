483158bf7514355e58351cdd2422448ac22b5e78
Owen Gaydosz <owgydz@proton.me>
2026-01-30 01:28:14 -0500

    t2wifi: bulk commit of device tree

---
+++ lineage_t290.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, device/samsung/t290/device.mk)
+
+PRODUCT_NAME := lineage_t290
+PRODUCT_DEVICE := t290
+PRODUCT_MANUFACTURER := 
+PRODUCT_BRAND := samsung
